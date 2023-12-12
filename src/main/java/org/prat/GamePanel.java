1e3d372a76123d4bf29810fabac454abd8888dcf
Prateek <prateekt91@gmail.com>
2023-12-12 08:24:04 +0530

    [Added Random color on rectangle]

--- src/main/java/org/prat/GamePanel.java
+++ src/main/java/org/prat/GamePanel.java
@@ -5,16 +5,23 @@ import org.prat.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100;
-    private int yDelta = 100;
+    private float xDelta = 100;
+    private float xDir = 0.003f;
+    private float yDelta = 100;
+    private float yDir = 0.003f;
     private int frames = 0;
     private long lastCheck = 0;
+
+    private Color color  = new Color(150,20,90);
+    private Random random;
     public GamePanel() {
 
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
@@ -40,7 +47,10 @@ public class GamePanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillRect(xDelta, yDelta,200,50);
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int) xDelta, (int) yDelta,200,50);
+
         frames ++;
         if(System.currentTimeMillis() - lastCheck >=1000) {
             lastCheck = System.currentTimeMillis();
@@ -49,5 +59,26 @@ public class GamePanel extends JPanel {
         }
         repaint();
 
+    }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if(xDelta > 400 || xDelta <0){
+            xDir *= -1;
+            color = getRandomColor();
+        }
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta <0){
+            yDir *= -1;
+            color = getRandomColor();
+        }
+    }
+
+    private Color getRandomColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return new Color(r,g,b);
     }
 }
