aa86334052523670eb582509f9e9f7b42e468d42
Prateek <prateekt91@gmail.com>
2023-12-14 22:23:11 +0530

    [Removed Rectangle code]

--- src/main/java/org/prat/GamePanel.java
+++ src/main/java/org/prat/GamePanel.java
@@ -5,7 +5,6 @@ import org.prat.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
 public class GamePanel extends JPanel {
 
@@ -17,15 +16,20 @@ public class GamePanel extends JPanel {
     private int frames = 0;
     private long lastCheck = 0;
 
-    private Color color  = new Color(150,20,90);
-    private Random random;
     public GamePanel() {
 
-        random = new Random();
         mouseInputs = new MouseInputs(this);
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280,800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
     }
 
     public void changeXDelta(int value) {
@@ -35,42 +39,12 @@ public class GamePanel extends JPanel {
 
     public void changeYDelta(int value) {
         yDelta += value;
-        //repaint();
     }
 
-    public void setRectPos(int x, int y) {
-
-        this.xDelta = x;
-        this.yDelta = y;
-        //repaint();
-    }
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect((int) xDelta, (int) yDelta,200,50);
 
     }
 
-    private void updateRectangle() {
-        xDelta += xDir;
-        if(xDelta > 400 || xDelta <0){
-            xDir *= -1;
-            color = getRandomColor();
-        }
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta <0){
-            yDir *= -1;
-            color = getRandomColor();
-        }
-    }
-
-    private Color getRandomColor() {
-        int r = random.nextInt(256);
-        int g = random.nextInt(256);
-        int b = random.nextInt(256);
-
-        return new Color(r,g,b);
-    }
 }
--- src/main/java/org/prat/GameWindow.java
+++ src/main/java/org/prat/GameWindow.java
@@ -7,10 +7,12 @@ public class GameWindow {
     private JFrame jFrame;
     public GameWindow(GamePanel gamePanel) {
         jFrame = new JFrame();
-        jFrame.setSize(400,400);
+
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null); // To make the window come in centre of the screen (passing null)
+        jFrame.setResizable(false);
+        jFrame.pack();
         jFrame.setVisible(true);
     }
 }
