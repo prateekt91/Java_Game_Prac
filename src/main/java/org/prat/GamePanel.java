80229dd42b9c83e12d6ff1e0e54852cbc633c0c7
Prateek <prateekt91@gmail.com>
2023-12-14 23:10:36 +0530

    [Adding image]

--- src/main/java/org/prat/GamePanel.java
+++ src/main/java/org/prat/GamePanel.java
@@ -3,8 +3,12 @@ package org.prat;
 import org.prat.inputs.KeyboardInputs;
 import org.prat.inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel {
 
@@ -16,13 +20,24 @@ public class GamePanel extends JPanel {
     private int frames = 0;
     private long lastCheck = 0;
 
+    private BufferedImage img, subImg;
+
     public GamePanel() {
 
         mouseInputs = new MouseInputs(this);
+        importImg();
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+
+    private void importImg() {
+        try (InputStream is = getClass().getResourceAsStream("/player_sprites.png")){
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void setPanelSize() {
@@ -41,10 +56,18 @@ public class GamePanel extends JPanel {
         yDelta += value;
     }
 
+    public void setRectPos(int x, int y) {
+
+        this.xDelta = x;
+        this.yDelta = y;
+        //repaint();
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        subImg = img.getSubimage(1*64,8*40,64,40);
+        g.drawImage(subImg, (int) xDelta, (int) yDelta,128,80, null);
     }
 
 }
